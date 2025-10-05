94c62d5c2440a3eda1680113ea3770888b2c22af
Echo Reader <tom.bisnis@gmail.com>
2025-10-05 15:24:29 +0700

    Create next-sitemap.config.js

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+// filepath: c:\Users\tekos\OneDrive\Documents\VisualCodeProjects\nextjs-blog-echo-reader\next-sitemap.config.js
+module.exports = {
+  siteUrl: 'https://echoreader.netlify.app', // ganti dengan domain kamu
+  generateRobotsTxt: true,
+}
