38bb29092ff2e4e232e5d4fcc0ed118c7c417052
echoreader <tom.bisnis@gmail.com>
2025-10-27 21:20:28 +0700

    Modular Sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
-module.exports = {
-  siteUrl: 'https://craftflavor.blog', // Tanpa trailing slash
-  generateRobotsTxt: true,
-  trailingSlash: false, // Hindari double slash
-}
+//module.exports = {
+  //siteUrl: 'https://craftflavor.blog', // Tanpa trailing slash
+  //generateRobotsTxt: true,
+  //trailingSlash: false, // Hindari double slash
+//}
 //echoreader.netlify.app > craftflavor.blog
