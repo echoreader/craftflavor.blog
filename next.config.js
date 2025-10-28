522106d7e5e40592ad5b37e17dfc3ca0c3e260a4
echoreader <tom.bisnis@gmail.com>
2025-10-29 01:05:37 +0700

    Add Cache

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
 };
-
 
 module.exports = nextConfig;
